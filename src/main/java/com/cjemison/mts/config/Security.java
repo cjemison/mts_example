f45d81921253083b6a28624aa3e58b258a8161f1
Cornelius Jemison <cjemison@gogoair.com>
2016-08-03 09:47:57 -0500

    added security.

---
+++ src/main/java/com/cjemison/mts/config/Security.java
@@ -0,0 +1,22 @@
+package com.cjemison.mts.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration
+      .WebSecurityConfigurerAdapter;
+
+/**
+ * Created by cjemison on 8/3/16.
+ */
+
+@EnableWebSecurity
+@Configuration
+public class Security extends WebSecurityConfigurerAdapter {
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests().anyRequest().fullyAuthenticated().and().
+          httpBasic().and().
+          csrf().disable();
+  }
+}
---
+++ src/main/java/com/cjemison/mts/config/WebSecurityConfiguration.java
@@ -0,0 +1,43 @@
+package com.cjemison.mts.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders
+      .AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers
+      .GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+/**
+ * Created by cjemison on 8/3/16.
+ */
+@Configuration
+public class WebSecurityConfiguration extends GlobalAuthenticationConfigurerAdapter {
+
+  @Value("${restUser}")
+  private String restUser;
+
+  @Value("${restPass}")
+  private String restPass;
+
+  @Override
+  public void init(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(userDetailsService());
+  }
+
+  @Bean
+  UserDetailsService userDetailsService() {
+    return username -> {
+      if (username.equals(restUser)) {
+        return new User(username, restPass, true, true, true, true,
+              AuthorityUtils.createAuthorityList("USER"));
+      }
+      throw new UsernameNotFoundException("could not find the user '"
+            + username + "'");
+    };
+  }
+}
